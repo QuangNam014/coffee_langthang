89c6a133de58a4fd638de58d1996abac583f35d8
QuangNam014 <tuquangnam1911@gmail.com>
2023-05-29 21:18:05 +0700

    update code lan 3

--- src/layouts/MainLayout/Container/Product.js
+++ src/layouts/MainLayout/Container/Product.js
@@ -4,6 +4,17 @@ function Product(props) {
     return (
         <div>
             <h1 style={{ fontSize: 50, textAlign: 'center', padding: '50px 0' }}>This is product</h1>
+            Product is abc
+            <table className="table table-hover" style={{ fontSize: 50 }}>
+                <tr>
+                    <th>id</th>
+                    <th>name</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>nam</td>
+                </tr>
+            </table>
         </div>
     );
 }
